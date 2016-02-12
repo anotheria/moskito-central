56271b59f6c5d28c859af1bf5248d209f5126448
BodyaVan <bodya.van@gmail.com>
2016-02-12 23:20:47 +0200

    refactor: make SnapshotMetaData getters in Builder style

--- moskito-central-core/src/main/java/org/moskito/central/SnapshotMetaData.java
+++ moskito-central-core/src/main/java/org/moskito/central/SnapshotMetaData.java
@@ -1,15 +1,14 @@
 package org.moskito.central;
 
-import java.io.Serializable;
-
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
 
 /**
  * Contains meta data about the snapshot like producerId, creation timestamp and
  * so on.
- * 
+ *
  * @author lrosenberg
  * @since 20.03.13 14:07
  */
@@ -54,9 +53,9 @@ public class SnapshotMetaData implements Serializable {
 	 * Subsystem of the producer.
 	 */
 	private String subsystem;
-	
+
 	/**
-	 * 
+	 *
 	 */
 	private String statClassName;
 
@@ -67,79 +66,88 @@ public class SnapshotMetaData implements Serializable {
 		arrivalTimestamp = System.currentTimeMillis();
 	}
 
-	public String getProducerId() {
-		return producerId;
-	}
+    public String getProducerId() {
+        return producerId;
+    }
 
-	public void setProducerId(String producerId) {
-		this.producerId = producerId;
-	}
+    public SnapshotMetaData setProducerId(String producerId) {
+        this.producerId = producerId;
+        return this;
+    }
 
-	public String getComponentName() {
-		return componentName;
-	}
+    public String getComponentName() {
+        return componentName;
+    }
 
-	public void setComponentName(String componentName) {
-		this.componentName = componentName;
-	}
+    public SnapshotMetaData setComponentName(String componentName) {
+        this.componentName = componentName;
+        return this;
+    }
 
-	public String getHostName() {
-		return hostName;
-	}
+    public String getHostName() {
+        return hostName;
+    }
 
-	public void setHostName(String hostName) {
-		this.hostName = hostName;
-	}
+    public SnapshotMetaData setHostName(String hostName) {
+        this.hostName = hostName;
+        return this;
+    }
 
-	public String getIntervalName() {
-		return intervalName;
-	}
+    public String getIntervalName() {
+        return intervalName;
+    }
 
-	public void setIntervalName(String intervalName) {
-		this.intervalName = intervalName;
-	}
+    public SnapshotMetaData setIntervalName(String intervalName) {
+        this.intervalName = intervalName;
+        return this;
+    }
 
-	public long getCreationTimestamp() {
-		return creationTimestamp;
-	}
+    public long getCreationTimestamp() {
+        return creationTimestamp;
+    }
 
-	public void setCreationTimestamp(long creationTimestamp) {
-		this.creationTimestamp = creationTimestamp;
-	}
+    public SnapshotMetaData setCreationTimestamp(long creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+        return this;
+    }
 
-	public long getArrivalTimestamp() {
-		return arrivalTimestamp;
-	}
+    public long getArrivalTimestamp() {
+        return arrivalTimestamp;
+    }
 
-	public void setArrivalTimestamp(long arrivalTimestamp) {
-		this.arrivalTimestamp = arrivalTimestamp;
-	}
+    public SnapshotMetaData setArrivalTimestamp(long arrivalTimestamp) {
+        this.arrivalTimestamp = arrivalTimestamp;
+        return this;
+    }
 
-	public String getCategory() {
-		return category;
-	}
+    public String getCategory() {
+        return category;
+    }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+    public SnapshotMetaData setCategory(String category) {
+        this.category = category;
+        return this;
+    }
 
-	public String getSubsystem() {
-		return subsystem;
-	}
+    public String getSubsystem() {
+        return subsystem;
+    }
 
-	public void setSubsystem(String subsystem) {
-		this.subsystem = subsystem;
-	}
+    public SnapshotMetaData setSubsystem(String subsystem) {
+        this.subsystem = subsystem;
+        return this;
+    }
 
-	public String getStatClassName() {
-		return statClassName;
-	}
+    public String getStatClassName() {
+        return statClassName;
+    }
 
-	public void setStatClassName(String statClassName) {
-		this.statClassName = statClassName;
-	}
+    public SnapshotMetaData setStatClassName(String statClassName) {
+        this.statClassName = statClassName;
+        return this;
+    }
 
-	@Override
+    @Override
 	public String toString() {
 		return "SnapshotMetaData [producerId=" + producerId + ", componentName=" + componentName + ", hostName=" + hostName + ", intervalName="
 				+ intervalName + ", creationTimestamp=" + creationTimestamp + ", arrivalTimestamp=" + arrivalTimestamp + ", category=" + category
