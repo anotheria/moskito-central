550a1f5e0de1ebfc629a3f3004dc1e4d4f4e141c
Andrii Skrypnyk <skrypnik.andrey@gmail.com>
2016-03-11 16:21:38 +0200

    refactor mongo storage

---
+++ moskito-central-storages/src/main/java/org/moskito/central/storage/mongo/MongoClientHolder.java
@@ -0,0 +1,40 @@
+package org.moskito.central.storage.mongo;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import org.moskito.central.Snapshot;
+
+import java.util.Arrays;
+
+/**
+ * @author andriiskrypnyk
+ */
+public enum MongoClientHolder {
+    INSTANCE;
+
+    private Gson gson = new GsonBuilder().create();
+
+    private MongoDBStorageConfig config;
+
+    private MongoClient client;
+
+    public void configure(MongoDBStorageConfig config) {
+        this.config = config;
+        MongoCredential credential = MongoCredential.createCredential(config.getLogin(), config.getDbName(), config.getPassword().toCharArray());
+        client = new MongoClient(new ServerAddress(config.getHost(), Integer.parseInt(config.getPort())), Arrays.asList(credential));
+    }
+
+    public void storeSnapshot(Snapshot target) {
+        MongoDatabase db = client.getDatabase(config.getDbName());
+        MongoCollection collection = db.getCollection(config.getCollectionName());
+        String json = gson.toJson(target);
+        collection.insertOne(Document.parse(json));
+    }
+
+}
--- moskito-central-storages/src/main/java/org/moskito/central/storage/mongo/MongoDBStorage.java
+++ moskito-central-storages/src/main/java/org/moskito/central/storage/mongo/MongoDBStorage.java
@@ -1,20 +1,10 @@
 package org.moskito.central.storage.mongo;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 import org.configureme.ConfigurationManager;
 import org.moskito.central.Snapshot;
 import org.moskito.central.storage.Storage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 
 /**
  * @author andriiskrypnyk
@@ -25,9 +15,6 @@ public class MongoDBStorage implements Storage {
 
     private MongoDBStorageConfig config;
 
-    private Gson gson = new GsonBuilder().create();
-
-
     @Override
     public void configure(String configurationName) {
 
@@ -37,6 +24,7 @@ public class MongoDBStorage implements Storage {
             return;
         try {
             ConfigurationManager.INSTANCE.configureAs(config, configurationName);
+            MongoClientHolder.INSTANCE.configure(config);
         } catch (IllegalArgumentException e) {
             log.warn("Couldn't configure MongoDBStorage with " + configurationName + " , working with default values");
         }
@@ -47,7 +35,6 @@ public class MongoDBStorage implements Storage {
     public void processSnapshot(Snapshot target) {
         String producerId = target.getMetaData().getProducerId();
         String interval = target.getMetaData().getIntervalName();
-        String collectionName = config.getCollectionName();
 
         if (!config.include(producerId, interval)) {
             return;
@@ -61,22 +48,10 @@ public class MongoDBStorage implements Storage {
         * store snapshots in different collections(ProducerId = collection name)
         * */
         if (config.getDistributeProducers().equals("true")) {
-            collectionName = target.getMetaData().getProducerId();
+            config.setCollectionName(target.getMetaData().getProducerId());
         }
 
-        MongoCredential credential = MongoCredential.createCredential(config.getLogin(), config.getDbName(), config.getPassword().toCharArray());
-        MongoClient mongoClient = null;
-
-        try {
-            mongoClient = new MongoClient(new ServerAddress(config.getHost(), Integer.parseInt(config.getPort())), Arrays.asList(credential));
-            MongoDatabase db = mongoClient.getDatabase(config.getDbName());
-            MongoCollection collection = db.getCollection(collectionName);
-            String json = gson.toJson(target);
-            collection.insertOne(Document.parse(json));
-        } finally {
-            mongoClient.close();
-        }
-
+        MongoClientHolder.INSTANCE.storeSnapshot(target);
     }
 
 }
