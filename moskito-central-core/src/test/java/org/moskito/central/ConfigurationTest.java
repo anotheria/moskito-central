490c59c27296fd35cffb97922e74916ecc0af3c1
Leon Rosenberg <leon.rosenberg@anotheria.net>
2017-05-09 17:00:27 +0200

    slight improvements in logging, removed obsolete configurations

--- moskito-central-core/src/test/java/org/moskito/central/ConfigurationTest.java
+++ moskito-central-core/src/test/java/org/moskito/central/ConfigurationTest.java
@@ -1,24 +1,23 @@
 package org.moskito.central;
 
-import org.moskito.central.Central;
+import org.junit.Test;
 import org.moskito.central.config.Configuration;
 import org.moskito.central.config.StorageConfigEntry;
-import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-/**
- * TODO comment this class
- *
- * @author lrosenberg
- * @since 22.03.13 11:53
- */
 public class ConfigurationTest {
 	@Test
-	public void testTestConfiguration(){
+	public void testConfiguration(){
 		Configuration config = Central.getInstance().getConfiguration();
 		StorageConfigEntry[] entries = config.getStorages();
 		assertNotNull(entries);
-		//System.out.println(Arrays.toString(entries));
+		assertEquals(3, entries.length);
+
+		assertEquals("json-file", entries[0].getName());
+		assertEquals("csv-file", entries[1].getName());
+		assertEquals("psql-db", entries[2].getName());
+
 	}
 }
